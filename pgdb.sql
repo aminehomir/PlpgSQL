efd5b05d6b1f2dee9e4b66ab73c08e529cfab6a6
aminehomir <57252890+aminehomir@users.noreply.github.com>
2021-04-25 20:41:37 +0000

    Add files via upload
    done

---
+++ pgdb.sql
@@ -0,0 +1,11 @@
+P765,Mohammed ahmed,Safi,JEE,DWWM,8,f
+P980,Don Reda,Safi,JEE,DWWM,8,f
+P307,Zakaria zakaria,Safi,FEBE,CDA,14,f
+P199,Omar omar,Safi,JEE,AI,10,f
+P387,Houssam houssam,Safi,FEBE,CDA,14,f
+P400,KAMAL BHF,Youssoufia,FEBE,CDA,14,f
+P543,Salma Salma,Youssoufia,C#,AI,10,f
+P566,Imane imane,Youssoufia,FEBE,CDA,14,f
+P122,Amine amine,Safi,DATA BI,CDA,14,f
+P202,Yassine yassine,Youssoufia,DATA BI,CDA,14,f
+P286,HOMIR,SAFI,FEBE,CDA,14,f
